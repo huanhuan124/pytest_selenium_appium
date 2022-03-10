b3982356314ecaeab09f7674e65ca6f6d60db721
zenghuan <zenghuan@zuche.com>
2022-03-10 15:20:33 +0800

    python  yaml文件读取 练习

--- wework_po/testcase/test_yml.py
+++ wework_po/testcase/test_yml.py
@@ -1,10 +1,20 @@
+import os.path
+
 import yaml
 
 
 def test_yml():
-    #C:\Users\zenghuan\PycharmProjects\test_selenium_appium\wework_po\datas\datas
+    #表示当前所处的文件夹的绝对路径
+    print(os.path.abspath('.'))
+    # 表示当前所处的文件夹上一级文件夹的绝对路径
+    print(os.path.abspath('..'))
 
-    with open("C:/Users/zenghuan/PycharmProjects/test_selenium_appium/wework_po/datas/datas.yml", 'r',encoding='utf-8') as f:
+    #C:\Users\zenghuan\PycharmProjects\test_selenium_appium\wework_po\datas\datas
+    print(os.path.abspath('./datas'))
+
+    # with open("C:/Users/zenghuan/PycharmProjects/test_selenium_appium/wework_po/datas/datas.yml", 'r',encoding='utf-8') as f:
+    # 使用相对路径读取yaml文件
+    with open("../datas/datas.yml", 'r',encoding='utf-8') as f:
         result = yaml.safe_load(f)
     print(result)
     print(result.get("datas"))
