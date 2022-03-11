dbfab6985cbd7b5c3f9285f8886c763aa6fa0fc7
zenghuan <zenghuan@zuche.com>
2022-03-11 18:48:49 +0800

    python  排序

--- pythontest/test_high_frequency.py
+++ pythontest/test_high_frequency.py
@@ -63,8 +63,46 @@ class Test_High_Frequency:
             print("path 不存在")
 
 
-    def test_sort(self):
+    def test_remove_sort(self):
         """
-        s = “ajldjlajfdljfddd”，去重并从小到大排序输出”adfjl”
+        s = "ajldjljfd"，去重并从小到大排序输出"adfjl"
         :return:
         """
+
+        s = "ajldjljfd"
+
+        # 方法1 去重连成字符串，然后转为list排序，再转为字符串
+        # res = ''
+        # #去重连成字符串
+        # for i in range(len(s)):
+        #     if s[i] not in res:
+        #         res = res+s[i]
+        # print(res)
+        # #字符串转换为列表
+        # list_res = list(res)
+        # print(list_res)
+        # #列表正序排列
+        # list_res.sort()
+        # print(list_res)
+        # #去重后倒序排列
+        # # list_res.sort(reverse=True)
+        # # print(list_res)
+        # #列表转换为字符串
+        # str_res = ''.join(list_res)
+        # print(str_res)
+
+        #方法2 用set先去掉重复元素，然后转为list排序，再转为字符串
+        s_set = set(s)
+        print(s_set)
+        s_list = list(s_set)
+        s_list.sort()
+        print(s_list)
+        str_res = ''.join(s_list)
+        print(str_res)
+
+        if  1==1:
+            pass
+        elif 1==2:
+            pass
+        else:
+            pass
